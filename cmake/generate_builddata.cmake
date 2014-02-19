c2631c1979d071c49a2777cd3d49dee44766ec0f
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2014-02-19 09:29:25 +0100

    Allow api git commit to be overwritten by commandline (for automatic builds)

--- cmake/generate_builddata.cmake
+++ cmake/generate_builddata.cmake
@@ -1,16 +1,21 @@
 #!/bin/cmake
 
 # look for git executable
-SET(found_git false) 
-find_program(found_git git)
+IF(NOT OONF_LIB_GIT)
+  SET(found_git false) 
+  find_program(found_git git)
 
-SET(OONF_LIB_GIT "cannot read git repository")
+  SET(OONF_LIB_GIT "cannot read git repository")
 
-IF(NOT ${found_git} STREQUAL "found_git-NOTFOUND")
-	# get git description WITH dirty flag
-	execute_process(COMMAND git describe --always --long --tags --dirty --match "v[0-9]*"
-		OUTPUT_VARIABLE OONF_LIB_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
+  IF(NOT ${found_git} STREQUAL "found_git-NOTFOUND")
+	  # get git description WITH dirty flag
+	  execute_process(COMMAND git describe --always --long --tags --dirty --match "v[0-9]*"
+		  OUTPUT_VARIABLE OONF_LIB_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
+  ENDIF()
+  
 ENDIF()
+
+message ("Git commit: ${OONF_LIB_GIT}")
 
 # create builddata file
 configure_file (${SRC} ${DST})
