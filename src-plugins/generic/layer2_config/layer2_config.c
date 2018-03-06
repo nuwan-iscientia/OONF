3b3b5736829083e05cbb48610f8caa3874137a0f
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2018-03-06 09:54:04 +0100

    Fix parameter check for l2neighbor/l2default parameter

--- src-plugins/generic/layer2_config/layer2_config.c
+++ src-plugins/generic/layer2_config/layer2_config.c
@@ -129,12 +129,12 @@ static struct cfg_schema_entry _l2net_entries[] = {
   CFG_MAP_STRING_ARRAY(l2_config_data, txt_value, "l2net_value", "", "Layer2 network value", _MAX_L2_VALUE_LEN),
 };
 static struct cfg_schema_entry _l2net_def_entries[] = {
-  CFG_MAP_CHOICE_L2NEIGH(l2_config_data, data_idx, "l2net_key", "", "Layer2 neighbor key for configuration"),
+  CFG_MAP_CHOICE_L2NEIGH(l2_config_data, data_idx, "l2neigh_key", "", "Layer2 neighbor key for configuration"),
   CFG_MAP_STRING_ARRAY(
-    l2_config_data, txt_value, "l2net_value", "", "Layer2 neighbor value for default neighbor data", _MAX_L2_VALUE_LEN),
+    l2_config_data, txt_value, "l2neigh_value", "", "Layer2 neighbor value for default neighbor data", _MAX_L2_VALUE_LEN),
 };
 static struct cfg_schema_entry _l2neigh_entries[] = {
-  CFG_MAP_CHOICE_L2NET(l2_config_data, data_idx, "l2neigh_key", "", "Layer2 neighbor key for configuration"),
+  CFG_MAP_CHOICE_L2NEIGH(l2_config_data, data_idx, "l2neigh_key", "", "Layer2 neighbor key for configuration"),
   CFG_MAP_NETADDR_MAC48(l2_config_data, mac, "l2neigh_mac", "", "MAC address of neighbor", false, false),
   CFG_MAP_STRING_ARRAY(l2_config_data, txt_value, "l2neigh_value", "", "Layer2 neighbor value", _MAX_L2_VALUE_LEN),
 };
